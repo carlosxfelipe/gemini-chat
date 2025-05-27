92076192c29d12d3f3b9303f7e10446acef0c4db
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-05-26 22:18:54 -0300

    Refactor normalize function to include additional text substitutions for improved normalization

--- src/utils/normalize.ts
+++ src/utils/normalize.ts
@@ -1,6 +1,32 @@
 // Função para remover acentos e normalizar
-export const normalize = (text: string) =>
-  text
+// export const normalize = (text: string) =>
+//   text
+//     .toLowerCase()
+//     .normalize("NFD")
+//     .replace(/[\u0300-\u036f]/g, "");
+
+export const normalize = (text: string) => {
+  const substitutions: Record<string, string> = {
+    "\\beh\\b": "é",
+    "\\bpq\\b": "porque",
+    "\\bvc\\b": "você",
+    "\\btb\\b": "também",
+    "\\btd\\b": "tudo",
+    "\\bblz\\b": "beleza",
+    "\\bta\\b": "tá",
+    "\\bobg\\b": "obrigado",
+    "\\btmj\\b": "tamo junto",
+    "\\blgl\\b": "legal",
+  };
+
+  let normalized = text
     .toLowerCase()
     .normalize("NFD")
     .replace(/[\u0300-\u036f]/g, "");
+
+  for (const [pattern, replacement] of Object.entries(substitutions)) {
+    normalized = normalized.replace(new RegExp(pattern, "g"), replacement);
+  }
+
+  return normalized;
+};
